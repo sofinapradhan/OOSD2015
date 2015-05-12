79bbe8f4807c792e20d9fc2bccdd242eeb052770
linden-peters <linden-peters@users.noreply.github.com>
2015-05-12 14:26:36 -0600

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+<?php
+/* Written by lpeters
+ * Date: 2015/05/12
+ * Usage: Include at bottom of page.
+ */
+?>
+<footer>
+&copy; <?= date("Y"); ?> A Travel Agency. All rights reserved.
+</footer>
+</body>
+</html>
