a50e434fcdf47b26eee81a0b66f4844b4c6a2914
linden-peters <504138@ELLPOOSD206886.ACDM.DS.SAIT.CA>
2015-05-15 15:09:49 -0600

    Contribution Merge
    Merged all contributions to work with shared header.

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php
 /* Written by lpeters
- * Style by spradhan
+ * Style by dostapenko
  * Date: 2015/05/12
  * Usage: Include at head of page.
  *     Set variables prior to include:
@@ -16,50 +16,34 @@
 <meta charset='UTF-8' />
 <title><?= $header_title ?></title>
 <link href='global.css' rel='stylesheet' type='text/css' />
-<link href='menu.css' rel='stylesheet' type='text/css' />
+<link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css' rel='stylesheet' type='text/css' /> <!-- Latest compiled and minified CSS -->
 <?php
 if (isset($header_style))
 {
 	foreach($header_style as $stylesheet)
 	{
-		print("<link href='$stylesheet' rel='stylesheet' type='text/css' />");
+		print("<link href='$stylesheet' rel='stylesheet' type='text/css' />\n");
 	}
 }
 if (isset($header_script))
 {
 	foreach($header_script as $script)
 	{
-		print("<script src='$script'></script>");
+		print("<script src='$script'></script>\n");
 	}
 }
 ?>
 </head>
 <body>
-<header>
-	<div id='companylogo'> </div>   <!-- this is for logo -->
-	<div id='header1'>
-		<table width='1010' height='10'>
-			<tr>
-			<td align='right' width='60%'><img src='img/map.jpg' height='30' width='30'>Map & direction</td>
-			<td align='right' width='15%'><img src='img/meeting.jpg' height='30' width='30'>Meetings</td>
-			<td align='center'><img src='img/photog.jpg' width='30' height='30'>Photo Gallery</td>
-			</tr>
-		</table>
-	</div>
-	<div id='header2'>
-		<div style='text-align:top;font-size:180%;width:100%;border:0px solid;'>Welcome to Travel Experts&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<font size='5%'>Toll free Reservations (866) 555 7897</font></div>
-	</div>
-	<div id='header3'>
-		<table>
-			<tr>
-			<td width='5%' align='center'><a href='index.php'>Home</a></td>
-			<td width='9%' align='center'><a href='packages.php'>Packages</a></td>
-			<td width='10%' align='center'><a href='register.php'>Register</a></td>
-			<td width='10%' align='center'><a href='vacation.php'>Vacation</a></td>
-			<td width='10%' align='center'><a href='contact.php'>Contact</a></td>
-			<td width='10%' align='center'><a href='about.php'>About</a></td>
-			</tr>
-		</table>
-		<hr>
-	</div>
+<header id='titleheader'>
+	<h1>Welcome to Travel Experts!&trade;</h1>
 </header>
+<nav id='navBar'>
+	<ul>
+		<li><a href='index.php' class='button'>Home</a></li>
+		<li><a href='packages.php' class='button'>Travel Packages</a></li>
+		<li><a href='register.php' class='button'>Register</a></li>
+		<li><a href='about.php' class='button'>About / FAQ</a></li>
+		<li><a href='contact.php' class='button'>Contact</a></li>
+	</ul>
+</nav>
