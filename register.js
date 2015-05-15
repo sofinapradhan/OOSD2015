5cafbd7e8cc6d3ed472fc6b27d8b96cbc509c122
linden-peters <linden-peters@users.noreply.github.com>
2015-05-15 10:47:51 -0600

    Update register.js
    Removed formHintList, it serves no purpose at this time.
    Added basic validation for all new fields.

--- register.js
+++ register.js
@@ -2,14 +2,6 @@
  * Date: 2015/05/12
  * Usage: Included by register.php.
  */
-formHintList = [
-	{key: "custname", value: "Customer's First Name."},
-	{key: "custaddy", value: "Customer's Last Name."},
-	{key: "custcity", value: "Customer's Home City."},
-	{key: "custprov", value: "Customer's Home Province."},
-	{key: "custpost", value: "Customer's Postal Code."},
-	{key: "destination", value: "Travel Destination."},
-	{key: "paymethod", value: "Monetary Compensation Transaction Protocol."}];
 	
 function checkForm()
 {
@@ -17,15 +9,20 @@ function checkForm()
 	var targForm = document.getElementById("regForm");
 	var errorFlag = false;
 	clearWarn();
-	if (targForm.custpnom.value == "")
+	if (targForm.custfirstname.value == "")
 	{
 		errorFlag = true;
-		document.getElementById("fb-custpnom").innerHTML = "Error: Full Name requires a value.";
+		document.getElementById("fb-custfirstname").innerHTML = "Error: First Name requires a value.";
 	}
-	if (targForm.custaddy.value == "")
+	if (targForm.custlastname.value == "")
 	{
 		errorFlag = true;
-		document.getElementById("fb-custaddy").innerHTML = "Error: Address requires a value.";
+		document.getElementById("fb-custlastname").innerHTML = "Error: Last Name requires a value.";
+	}
+	if (targForm.custaddress.value == "")
+	{
+		errorFlag = true;
+		document.getElementById("fb-custaddress").innerHTML = "Error: Address requires a value.";
 	}
 	if (targForm.custcity.value == "")
 	{
@@ -37,40 +34,78 @@ function checkForm()
 		errorFlag = true;
 		document.getElementById("fb-custprov").innerHTML = "Error: Province requires a value.";
 	}
-	//var re = new RegExp(/^[A-Z]\d[A-Z]\s?\d[A-Z]\d$/i);
-	if (targForm.custpost.value == "")
+	if (targForm.custcountry.value == "")
 	{
 		errorFlag = true;
-		document.getElementById("fb-custpost").innerHTML = "Error: Postal Code requires a value.";
+		document.getElementById("fb-custcountry").innerHTML = "Error: Country requires a value.";
+	}
+	//var re = new RegExp(/^[A-Z]\d[A-Z]\s?\d[A-Z]\d$/i);
+	if (targForm.custpostal.value == "")
+	{
+		errorFlag = true;
+		document.getElementById("fb-custpostal").innerHTML = "Error: Postal Code requires a value.";
 	}
 	//else if (!re.test(targForm.custpost.value))
-	else if (!new RegExp(/^[A-Z]\d[A-Z]\s?\d[A-Z]\d$/i).test(targForm.custpost.value))
+	else if (!new RegExp(/^[A-Z]\d[A-Z]\s?\d[A-Z]\d$/i).test(targForm.custpostal.value))
 	{
 		errorFlag = true;
-		document.getElementById("fb-custpost").innerHTML = "Error: Postal Code must be valid.";
+		document.getElementById("fb-custpostal").innerHTML = "Error: Postal Code must be valid.";
+	}
+	if (targForm.custhomephone.value == "")
+	{
+		errorFlag = true;
+		document.getElementById("fb-custhomephone").innerHTML = "Error: Home Phone requires a value.";
+	}
+	if (targForm.custbusphone.value == "")
+	{
+		errorFlag = true;
+		document.getElementById("fb-custbusphone").innerHTML = "Error: Work Phone requires a value.";
+	}
+	if (targForm.custemail.value == "")
+	{
+		errorFlag = true;
+		document.getElementById("fb-custemail").innerHTML = "Error: E-mail Address requires a value.";
 	}
 	if (targForm.destination.value == "")
 	{
 		errorFlag = true;
 		document.getElementById("fb-destination").innerHTML = "Error: Destination requires a value.";
 	}
-	if (targForm.paymethod.value == "")
+	if (targForm.ccname.value == "")
 	{
 		errorFlag = true;
-		document.getElementById("fb-paymethod").innerHTML = "Error: Payment Method requires a value.";
+		document.getElementById("fb-ccname").innerHTML = "Error: Credit Card Provider requires a value.";
+	}
+	if (targForm.ccnumber.value == "")
+	{
+		errorFlag = true;
+		document.getElementById("fb-ccnumber").innerHTML = "Error: Credit Card Number requires a value.";
+	}
+	if (targForm.ccexpiry.value == "")
+	{
+		errorFlag = true;
+		document.getElementById("fb-ccexpiry").innerHTML = "Error: Credit Card Expiry requires a value.";
 	}
 	console.log("errorFlag=" + errorFlag);
 	return (errorFlag === false && confirm("Are you sure you want to submit registration?"));
 }
 function clearWarn()
 {
-	document.getElementById("fb-custpnom").innerHTML = "";
-	document.getElementById("fb-custaddy").innerHTML = "";
+	console.log("Clearing warnings...");
+	document.getElementById("fb-custfirstname").innerHTML = "";
+	document.getElementById("fb-custlastname").innerHTML = "";
+	document.getElementById("fb-custaddress").innerHTML = "";
 	document.getElementById("fb-custcity").innerHTML = "";
 	document.getElementById("fb-custprov").innerHTML = "";
-	document.getElementById("fb-custpost").innerHTML = "";
+	document.getElementById("fb-custcountry").innerHTML = "";
+	document.getElementById("fb-custpostal").innerHTML = "";
+	document.getElementById("fb-custhomephone").innerHTML = "";
+	document.getElementById("fb-custbusphone").innerHTML = "";
+	document.getElementById("fb-custemail").innerHTML = "";
 	document.getElementById("fb-destination").innerHTML = "";
-	document.getElementById("fb-paymethod").innerHTML = "";
+	document.getElementById("fb-ccname").innerHTML = "";
+	document.getElementById("fb-ccnumber").innerHTML = "";
+	document.getElementById("fb-ccexpiry").innerHTML = "";
 }
 function showHint(fieldName)
 {
