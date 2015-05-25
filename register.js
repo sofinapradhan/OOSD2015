23c0b54dbb7957185c33aa0379a0b72190d24007
linden-peters <504138@ICTVM-8B3KR4FV3.ACDM.DS.SAIT.CA>
2015-05-25 00:31:37 -0600

    Registration Form and Graphics
    Added SQL Insert and visual changes to registration form.
    Uploading promo graphics for index.php

--- register.js
+++ register.js
@@ -12,79 +12,135 @@ function checkForm()
 	if (targForm.custfirstname.value == "")
 	{
 		errorFlag = true;
-		document.getElementById("fb-custfirstname").innerHTML = "Error: First Name requires a value.";
+		document.getElementById("fb-custfirstname").innerHTML = "<img src='img/icon_fail.png' style='height:1em' title='Error: First Name requires a value.'>";
+	}
+	else
+	{
+		document.getElementById("fb-custfirstname").innerHTML = "<img src='img/icon_pass.png' style='height:1em'>";
 	}
 	if (targForm.custlastname.value == "")
 	{
 		errorFlag = true;
-		document.getElementById("fb-custlastname").innerHTML = "Error: Last Name requires a value.";
+		document.getElementById("fb-custlastname").innerHTML = "<img src='img/icon_fail.png' style='height:1em' title='Error: Last Name requires a value.'>";
+	}
+	else
+	{
+		document.getElementById("fb-custlastname").innerHTML = "<img src='img/icon_pass.png' style='height:1em'>";
 	}
 	if (targForm.custaddress.value == "")
 	{
 		errorFlag = true;
-		document.getElementById("fb-custaddress").innerHTML = "Error: Address requires a value.";
+		document.getElementById("fb-custaddress").innerHTML = "<img src='img/icon_fail.png' style='height:1em' title='Error: Address requires a value.'>";
+	}
+	else
+	{
+		document.getElementById("fb-custaddress").innerHTML = "<img src='img/icon_pass.png' style='height:1em'>";
 	}
 	if (targForm.custcity.value == "")
 	{
 		errorFlag = true;
-		document.getElementById("fb-custcity").innerHTML = "Error: City requires a value.";
+		document.getElementById("fb-custcity").innerHTML = "<img src='img/icon_fail.png' style='height:1em' title='Error: City requires a value.'>";
+	}
+	else
+	{
+		document.getElementById("fb-custcity").innerHTML = "<img src='img/icon_pass.png' style='height:1em'>";
 	}
 	if (targForm.custprov.value == "")
 	{
 		errorFlag = true;
-		document.getElementById("fb-custprov").innerHTML = "Error: Province requires a value.";
+		document.getElementById("fb-custprov").innerHTML = "<img src='img/icon_fail.png' style='height:1em' title='Error: Province requires a value.'>";
+	}
+	else
+	{
+		document.getElementById("fb-custprov").innerHTML = "<img src='img/icon_pass.png' style='height:1em'>";
 	}
 	if (targForm.custcountry.value == "")
 	{
 		errorFlag = true;
-		document.getElementById("fb-custcountry").innerHTML = "Error: Country requires a value.";
+		document.getElementById("fb-custcountry").innerHTML = "<img src='img/icon_fail.png' style='height:1em' title='Error: Country requires a value.'>";
+	}
+	else
+	{
+		document.getElementById("fb-custcountry").innerHTML = "<img src='img/icon_pass.png' style='height:1em'>";
 	}
 	//var re = new RegExp(/^[A-Z]\d[A-Z]\s?\d[A-Z]\d$/i);
 	if (targForm.custpostal.value == "")
 	{
 		errorFlag = true;
-		document.getElementById("fb-custpostal").innerHTML = "Error: Postal Code requires a value.";
+		document.getElementById("fb-custpostal").innerHTML = "<img src='img/icon_fail.png' style='height:1em' title='Error: Postal Code requires a value.'>";
 	}
 	//else if (!re.test(targForm.custpost.value))
 	else if (!new RegExp(/^[A-Z]\d[A-Z]\s?\d[A-Z]\d$/i).test(targForm.custpostal.value))
 	{
 		errorFlag = true;
-		document.getElementById("fb-custpostal").innerHTML = "Error: Postal Code must be valid.";
+		document.getElementById("fb-custpostal").innerHTML = "<img src='img/icon_fail.png' style='height:1em' title='Error: Postal Code must be valid.'>";
+	}
+	else
+	{
+		document.getElementById("fb-custpostal").innerHTML = "<img src='img/icon_pass.png' style='height:1em'>";
 	}
 	if (targForm.custhomephone.value == "")
 	{
 		errorFlag = true;
-		document.getElementById("fb-custhomephone").innerHTML = "Error: Home Phone requires a value.";
+		document.getElementById("fb-custhomephone").innerHTML = "<img src='img/icon_fail.png' style='height:1em' title='Error: Home Phone requires a value.'>";
+	}
+	else
+	{
+		document.getElementById("fb-custhomephone").innerHTML = "<img src='img/icon_pass.png' style='height:1em'>";
 	}
 	if (targForm.custbusphone.value == "")
 	{
 		errorFlag = true;
-		document.getElementById("fb-custbusphone").innerHTML = "Error: Work Phone requires a value.";
+		document.getElementById("fb-custbusphone").innerHTML = "<img src='img/icon_fail.png' style='height:1em' title='Error: Work Phone requires a value.'>";
+	}
+	else
+	{
+		document.getElementById("fb-custbusphone").innerHTML = "<img src='img/icon_pass.png' style='height:1em'>";
 	}
 	if (targForm.custemail.value == "")
 	{
 		errorFlag = true;
-		document.getElementById("fb-custemail").innerHTML = "Error: E-mail Address requires a value.";
+		document.getElementById("fb-custemail").innerHTML = "<img src='img/icon_fail.png' style='height:1em' title='Error: E-mail Address requires a value.'>";
+	}
+	else
+	{
+		document.getElementById("fb-custemail").innerHTML = "<img src='img/icon_pass.png' style='height:1em'>";
 	}
 	if (targForm.destination.value == "")
 	{
 		errorFlag = true;
-		document.getElementById("fb-destination").innerHTML = "Error: Destination requires a value.";
+		document.getElementById("fb-destination").innerHTML = "<img src='img/icon_fail.png' style='height:1em' title='Error: Destination requires a value.'>";
+	}
+	else
+	{
+		document.getElementById("fb-destination").innerHTML = "<img src='img/icon_pass.png' style='height:1em'>";
 	}
 	if (targForm.ccname.value == "")
 	{
 		errorFlag = true;
-		document.getElementById("fb-ccname").innerHTML = "Error: Credit Card Provider requires a value.";
+		document.getElementById("fb-ccname").innerHTML = "<img src='img/icon_fail.png' style='height:1em' title='Error: Credit Card Provider requires a value.'>";
+	}
+	else
+	{
+		document.getElementById("fb-ccname").innerHTML = "<img src='img/icon_pass.png' style='height:1em'>";
 	}
 	if (targForm.ccnumber.value == "")
 	{
 		errorFlag = true;
-		document.getElementById("fb-ccnumber").innerHTML = "Error: Credit Card Number requires a value.";
+		document.getElementById("fb-ccnumber").innerHTML = "<img src='img/icon_fail.png' style='height:1em' title='Error: Credit Card Number requires a value.'>";
+	}
+	else
+	{
+		document.getElementById("fb-ccnumber").innerHTML = "<img src='img/icon_pass.png' style='height:1em'>";
 	}
 	if (targForm.ccexpiry.value == "")
 	{
 		errorFlag = true;
-		document.getElementById("fb-ccexpiry").innerHTML = "Error: Credit Card Expiry requires a value.";
+		document.getElementById("fb-ccexpiry").innerHTML = "<img src='img/icon_fail.png' style='height:1em' title='Error: Credit Card Expiry requires a value.'>";
+	}
+	else
+	{
+		document.getElementById("fb-ccexpiry").innerHTML = "<img src='img/icon_pass.png' style='height:1em'>";
 	}
 	console.log("errorFlag=" + errorFlag);
 	return (errorFlag === false && confirm("Are you sure you want to submit registration?"));
--- register.php
+++ register.php
@@ -15,10 +15,18 @@ if (isset($_POST['gobutton']))
 	{
 		print("<p>$field=$value</p>");
 	}
+	if (isset($_POST['custfirstname'],$_POST['custlastname'],$_POST['custaddress'],$_POST['custcity'],$_POST['custprov'],$_POST['custcountry'],$_POST['custpostal'],$_POST['custhomephone'],$_POST['custbusphone'],$_POST['custemail']))
+	{
+		print "<p>All required information present, uploading to database.</p>";
+		/* This is only temporary, replace it with prepared statements and proper input sanitation. */
+		$sql = "INSERT INTO `customers` (`CustomerId`, `CustFirstName`, `CustLastName`, `CustAddress`, `CustCity`, `CustProv`, `CustCountry`, `CustPostal`, `CustHomePhone`, `CustBusPhone`, `CustEmail`, `AgentId`) VALUES (NULL, '{$_POST['custfirstname']}', '{$_POST['custlastname']}', '{$_POST['custaddress']}', '{$_POST['custcity']}', '{$_POST['custprov']}', '{$_POST['custcountry']}', '{$_POST['custpostal']}', '{$_POST['custhomephone']}', '{$_POST['custbusphone']}', '{$_POST['custemail']}', NULL);";
+		$result = mysqli_query($con, $sql) or die('Query Failed: ' . mysqli_error($con));
+		print "<p>$result</p>";
+	}
 }
 else
 {
-	print("<article>
+/*
 <p id='fh-custfirstname' class='formHint'>Customer's First Name.</p>
 <p id='fh-custlastname' class='formHint'>Customer's Last Name.</p>
 <p id='fh-custaddress' class='formHint'>Customer's Home Address.</p>
@@ -33,13 +41,28 @@ else
 <p id='fh-ccname' class='formHint'>Credit Card Provider.</p>
 <p id='fh-ccnumber' class='formHint'>Credit Card Number.</p>
 <p id='fh-ccexpiry' class='formHint'>Credit Card Expiration.</p>
+*/
+print("<article>
 <form id='regForm' name='register' method='post' action='{$_SERVER['PHP_SELF']}' onReset='clearWarn();'>
 <table>
-<tr><td><label for='custfirstname'>First Name:</label></td><td><input type='text' name='custfirstname' onFocus='showHint(this.name);' onBlur='hideHint(this.name);' /></td><td id='fb-custfirstname'></td></tr>
-<tr><td><label for='custlastname'>Last Name:</label></td><td><input type='text' name='custlastname' onFocus='showHint(this.name);' onBlur='hideHint(this.name);' /></td><td id='fb-custlastname'></td></tr>
-<tr><td><label for='custaddress'>Address:</label></td><td><input type='text' name='custaddress' onFocus='showHint(this.name);' onBlur='hideHint(this.name);' /></td><td id='fb-custaddress'></td></tr>
-<tr><td><label for='custcity'>City:</label></td><td><input type='text' name='custcity' onFocus='showHint(this.name);' onBlur='hideHint(this.name);' /></td><td id='fb-custcity'></td></tr>
-<tr><td><label for='custprov'>Province:</label></td><td><select name='custprov' onFocus='showHint(this.name);' onBlur='hideHint(this.name);'>
+<tr><td><label for='custfirstname'>First Name:</label></td>
+	<td><input type='text' name='custfirstname' onFocus='showHint(this.name);' onBlur='hideHint(this.name);' /></td>
+	<td id='fi-custfirstname'><img src='img/icon_info.png' style='height:1em' title='Customers First Name'></td>
+	<td id='fb-custfirstname'></td></tr>
+<tr><td><label for='custlastname'>Last Name:</label></td>
+	<td><input type='text' name='custlastname' onFocus='showHint(this.name);' onBlur='hideHint(this.name);' /></td>
+	<td id='fi-custlastname'><img src='img/icon_info.png' style='height:1em' title='Customers Last Name'></td>
+	<td id='fb-custlastname'></td></tr>
+<tr><td><label for='custaddress'>Address:</label></td>
+	<td><input type='text' name='custaddress' onFocus='showHint(this.name);' onBlur='hideHint(this.name);' /></td>
+	<td id='fi-custaddress'><img src='img/icon_info.png' style='height:1em' title='Customers Address'></td>
+	<td id='fb-custaddress'></td></tr>
+<tr><td><label for='custcity'>City:</label></td>
+	<td><input type='text' name='custcity' onFocus='showHint(this.name);' onBlur='hideHint(this.name);' /></td>
+	<td id='fi-custcity'><img src='img/icon_info.png' style='height:1em' title='Customers Home City'></td>
+	<td id='fb-custcity'></td></tr>
+<tr><td><label for='custprov'>Province:</label></td>
+	<td><select name='custprov' onFocus='showHint(this.name);' onBlur='hideHint(this.name);'>
 	<option value=''>---- Select Province ----</option>
 	<option value='AB'>Alberta</option>
 	<option value='BC'>British Columbia</option>
@@ -54,28 +77,49 @@ else
 	<option value='QC'>Quebec</option>
 	<option value='SK'>Saskatchewan</option>
 	<option value='YT'>Yukon</option>
-	</select></td><td id='fb-custprov'></td></tr>
-<tr><td><label for='custcountry'>Country:</label></td><td><select name='custcountry' onFocus='showHint(this.name);' onBlur='hideHint(this.name);'>
+	</select></td>
+	<td id='fi-custprov'><img src='img/icon_info.png' style='height:1em' title='Customers Home Province'></td>
+	<td id='fb-custprov'></td></tr>
+<tr><td><label for='custcountry'>Country:</label></td>
+	<td><select name='custcountry' onFocus='showHint(this.name);' onBlur='hideHint(this.name);'>
 	<option value=''>---- Select Country ----</option>
 	<option value='CA'>Canada</option>
 	<option value='US'>United States of America</option>
-	</select></td><td id='fb-custcountry'></td></tr>
-<tr><td><label for='custpostal'>Postal Code:</label></td><td><input type='text' name='custpostal' 
+	</select></td>
+	<td id='fi-custcountry'><img src='img/icon_info.png' style='height:1em' title='Customers Home Country'></td>
+	<td id='fb-custcountry'></td></tr>
+<tr><td><label for='custpostal'>Postal Code:</label></td>
+	<td><input type='text' name='custpostal' 
 	maxlength='7'
 	onChange='this.value = this.value.toUpperCase();'
-	onFocus='showHint(this.name);' onBlur='hideHint(this.name);' /></td><td id='fb-custpostal'></td></tr>
-<tr><td><label for='custhomephone'>Home Phone:</label></td><td><input type='text' name='custhomephone' 
-	onFocus='showHint(this.name);' onBlur='hideHint(this.name);' /></td><td id='fb-custhomephone'></td></tr>
-<tr><td><label for='custbusphone'>Bus. Phone:</label></td><td><input type='text' name='custbusphone' 
-	onFocus='showHint(this.name);' onBlur='hideHint(this.name);' /></td><td id='fb-custbusphone'></td></tr>
-<tr><td><label for='custemail'>E-mail Address:</label></td><td><input type='text' name='custemail' 
-	onFocus='showHint(this.name);' onBlur='hideHint(this.name);' /></td><td id='fb-custemail'></td></tr>
-<tr><td><label for='destination'>Destination:</label></td><td><select name='destination' onFocus='showHint(this.name);' onBlur='hideHint(this.name);'>
+	onFocus='showHint(this.name);' onBlur='hideHint(this.name);' /></td>
+	<td id='fi-custpostal'><img src='img/icon_info.png' style='height:1em' title='Customers Postal Code'></td>
+	<td id='fb-custpostal'></td></tr>
+<tr><td><label for='custhomephone'>Home Phone:</label></td>
+	<td><input type='text' name='custhomephone' 
+	onFocus='showHint(this.name);' onBlur='hideHint(this.name);' /></td>
+	<td id='fi-custhomephone'><img src='img/icon_info.png' style='height:1em' title='Customers Home Phone'></td>
+	<td id='fb-custhomephone'></td></tr>
+<tr><td><label for='custbusphone'>Work Phone:</label></td>
+	<td><input type='text' name='custbusphone' 
+	onFocus='showHint(this.name);' onBlur='hideHint(this.name);' /></td>
+	<td id='fi-custbusphone'><img src='img/icon_info.png' style='height:1em' title='Customers Work Phone'></td>
+	<td id='fb-custbusphone'></td></tr>
+<tr><td><label for='custemail'>E-mail Address:</label></td>
+	<td><input type='text' name='custemail' 
+	onFocus='showHint(this.name);' onBlur='hideHint(this.name);' /></td>
+	<td id='fi-custemail'><img src='img/icon_info.png' style='height:1em' title='Customers E-mail Address'></td>
+	<td id='fb-custemail'></td></tr>
+<tr><td><label for='destination'>Destination:</label></td>
+	<td><select name='destination' onFocus='showHint(this.name);' onBlur='hideHint(this.name);'>
 	<option value=''>---- Go To ----</option>
 	<option value='cancun'>Cancun</option>
 	<option value='hawaii'>Hawaii</option>
 	<option value='leo'>Low Earth Orbit</option>
-	</select></td><td id='fb-destination'></td></tr>
+	<option value='four'>Option 4</option>
+	</select></td>
+	<td id='fi-destination'><img src='img/icon_info.png' style='height:1em' title='Travel Destination'></td>
+	<td id='fb-destination'></td></tr>
 <tr><td><label for='ccname'>Method of Payment:</label></td>
 	<td>
 	<input type='radio' name='ccname' value='AMEX' onFocus='showHint(this.name);' onBlur='hideHint(this.name);'><img src='img/card_amex.png' alt='American Express' /></input>
@@ -83,12 +127,19 @@ else
 	<input type='radio' name='ccname' value='MC' onFocus='showHint(this.name);' onBlur='hideHint(this.name);'><img src='img/card_mast.png' alt='MasterCard' /></input>
 	<input type='radio' name='ccname' value='VISA' onFocus='showHint(this.name);' onBlur='hideHint(this.name);'><img src='img/card_visa.png' alt='Visa' /></input>
 	</td>
+	<td id='fi-ccname'><img src='img/icon_info.png' style='height:1em' title='Credit Card Provider'></td>
 	<td id='fb-ccname'></td>
 	</tr>
-<tr><td><label for='ccnumber'>Card Number:</label></td><td><input type='text' name='ccnumber' 
-	onFocus='showHint(this.name);' onBlur='hideHint(this.name);' /></td><td id='fb-ccnumber'></td></tr>
-<tr><td><label for='ccexpiry'>Expiration:</label></td><td><input type='date' name='ccexpiry' 
-	onFocus='showHint(this.name);' onBlur='hideHint(this.name);' /></td><td id='fb-ccexpiry'></td></tr>
+<tr><td><label for='ccnumber'>Card Number:</label></td>
+	<td><input type='text' name='ccnumber' 
+	onFocus='showHint(this.name);' onBlur='hideHint(this.name);' /></td>
+	<td id='fi-ccnumber'><img src='img/icon_info.png' style='height:1em' title='Credit Card Number'></td>
+	<td id='fb-ccnumber'></td></tr>
+<tr><td><label for='ccexpiry'>Expiration:</label></td>
+	<td><input type='date' name='ccexpiry' 
+	onFocus='showHint(this.name);' onBlur='hideHint(this.name);' /></td>
+	<td id='fi-ccexpiry'><img src='img/icon_info.png' style='height:1em' title='Credit Card Expiration'></td>
+	<td id='fb-ccexpiry'></td></tr>
 <tr><td align='center'><input type='reset' name='rsbutton' value='Reset Form' onClick='return confirm(\"Are you sure you want to reset form?\");' /></td>
 	<td align='center'><input type='submit' name='gobutton' value='Submit Registration' onClick='return checkForm();' /></td><td></td></tr>
 </table>
